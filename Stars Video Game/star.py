3e8415ee1f577ee7a535069d88c94dce1d596672
Oliver <157323896+olivercooneymartin@users.noreply.github.com>
2026-04-16 11:15:22 -0400

    Add files via upload

---
+++ Stars Video Game/star.py
@@ -0,0 +1,17 @@
+#stars
+import pygame
+from pygame.sprite import Sprite 
+
+class Star(Sprite):
+    def __init__(self, stars_game):
+        super().__init__()
+        self.screen = stars_game.screen
+
+        self.image = pygame.image.load("images/star.bmp")
+        self.rect = self.image.get_rect()
+
+
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+        
+
