194b25b0e4a4af8d3e4473bddec2967822544766
Oliver <157323896+olivercooneymartin@users.noreply.github.com>
2026-04-27 15:17:12 -0400

    Update settings.py

--- Stars/settings.py
+++ Stars/settings.py
@@ -1,5 +1,10 @@
+# This file stores the settings for the Stars game.
+
 class Settings:
+    """A class to store game settings."""
+
     def __init__(self):
+        """Initialize the game's screen settings."""
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (50, 50, 50)
