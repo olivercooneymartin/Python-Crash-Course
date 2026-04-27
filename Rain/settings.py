15fdecf146419e770bed08b241516f7c19c6af80
Oliver <157323896+olivercooneymartin@users.noreply.github.com>
2026-04-27 15:50:08 -0400

    Update settings.py

--- Rain/settings.py
+++ Rain/settings.py
@@ -1,8 +1,15 @@
+# This file stores the settings for the Raindrops game.
+
 class Settings:
+    """A class to store game settings."""
 
     def __init__(self):
+        """Initialize the game's settings."""
+
+        # Screen settings.
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
 
+        # Raindrop settings.
         self.raindrop_speed = 1.5
